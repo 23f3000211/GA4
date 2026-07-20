2ebb0ca657a77a320593d09bcd128951a226b62e
23f3000211 <23f3000211@ds.study.iitm.ac.in>
2026-07-20 16:46:18 +0530

    Change AIPIPE_TOKEN in config.py
    Updated AIPIPE_TOKEN with a new token value.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # The only two things you must fill in:
 EMAIL = "your_email@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "PASTE_YOUR_AIPIPE_TOKEN"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjMwMDAyMTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDU0NjEzNCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTE1MDkzNH0.mSbR5BW-p0MQop93vRCehxQ66M7wrQGl5viywtFPzEs"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
